5dd7484372fb575a07f8b5e4e3773115924fc2ba
Weronika Żochowiec <Veronica.zochowiec@gmail.com>
2021-01-25 15:29:32 +0100

    dodanie modelu rezerwacji regularnych

--- reservations/models.py
+++ reservations/models.py
@@ -36,9 +36,31 @@ class Reservation(models.Model):
     accepted = AcceptedManager()  # Our custom manager.
 
 
+class RegularReservation(models.Model):
+    DAYS_OF_WEEK = (
+        (0, 'Monday'),
+        (1, 'Tuesday'),
+        (2, 'Wednesday'),
+        (3, 'Thursday'),
+        (4, 'Friday'),
+        (5, 'Saturday'),
+        (6, 'Sunday'),
+    )
+
+    name = models.CharField(max_length=40)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    day_of__week = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
+    reservations = models.ManyToManyField(Reservation, through='SingleRegularReservation')
+
+
+class SingleRegularReservation(models.Model):
+    regular_reservation_group = models.ForeignKey('RegularReservation', on_delete=models.CASCADE)
+    single_reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
+
+
 class ReservationType(models.Model):
     name = models.CharField(max_length=40)
-    regular = models.BooleanField(default=False)
 
     def __str__(self):
        return self.name
