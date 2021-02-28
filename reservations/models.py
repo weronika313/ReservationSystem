c0b6b8f8c4fd4664d81bdda9cf4e50d613a9245c
Weronika Żochowiec <Veronica.zochowiec@gmail.com>
2021-02-28 19:35:52 +0100

    dodanie panelu admina, poprawienie modeli

--- reservations/models.py
+++ reservations/models.py
@@ -7,27 +7,33 @@ from users.models import CustomUser as User
 
 class AcceptedManager(models.Manager):
     def get_queryset(self):
-        return super(AcceptedManager, self).get_queryset()\
-                .filter(status='accepted')
+        return super(AcceptedManager, self).get_queryset().filter(status="accepted")
+
+
+STATUS_CHOICES = (
+    ("waiting", "Waiting"),
+    ("accepted", "Accepted"),
+    ("rejected", "Rejected"),
+    )
 
 
 class Reservation(models.Model):
     STATUS_CHOICES = (
-        ('waiting', 'Waiting'),
-        ('accepted', 'Accepted'),
-        ('rejected', 'Rejected'),
+        ("waiting", "Waiting"),
+        ("accepted", "Accepted"),
+        ("rejected", "Rejected"),
     )
 
     start = models.DateTimeField(null=True, blank=True)
     end = models.DateTimeField(null=True, blank=True)
     amountOfPeople = models.IntegerField()
-    comment = models.TextField(max_length=200,)
+    comment = models.TextField(
+        max_length=200,
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey('Room', on_delete=models.CASCADE)
-    type = models.ForeignKey('ReservationType', on_delete=models.CASCADE)
-    status = models.CharField(max_length=10,
-                              choices=STATUS_CHOICES,
-                              default='waiting')
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    type = models.ForeignKey("ReservationType", on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="waiting")
 
     def __str__(self):
         return self.id
@@ -38,109 +44,112 @@ class Reservation(models.Model):
 
 class RegularReservation(models.Model):
     DAYS_OF_WEEK = (
-        (0, 'Monday'),
-        (1, 'Tuesday'),
-        (2, 'Wednesday'),
-        (3, 'Thursday'),
-        (4, 'Friday'),
-        (5, 'Saturday'),
-        (6, 'Sunday'),
+        (0, "Monday"),
+        (1, "Tuesday"),
+        (2, "Wednesday"),
+        (3, "Thursday"),
+        (4, "Friday"),
+        (5, "Saturday"),
+        (6, "Sunday"),
     )
 
     name = models.CharField(max_length=40)
     start_time = models.TimeField()
     end_time = models.TimeField()
-    day_of__week = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
-    reservations = models.ManyToManyField(Reservation, through='SingleRegularReservation')
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    day_of_week = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.ForeignKey("ReservationType", on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="waiting")
+    reservations = models.ManyToManyField(
+        Reservation, through="SingleRegularReservation"
+    )
 
 
 class SingleRegularReservation(models.Model):
-    regular_reservation_group = models.ForeignKey('RegularReservation', on_delete=models.CASCADE)
-    single_reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
+    regular_reservation_group = models.ForeignKey(
+        "RegularReservation", on_delete=models.CASCADE
+    )
+    single_reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
 
 
 class ReservationType(models.Model):
     name = models.CharField(max_length=40)
 
     def __str__(self):
-       return self.name
+        return self.name
 
 
 class Software(models.Model):
     name = models.CharField(max_length=40)
 
     def __str__(self):
-       return self.name
+        return self.name
 
 
 class Equipment(models.Model):
     name = models.CharField(max_length=40)
 
     def __str__(self):
-       return self.name
+        return self.name
 
 
 class Room(models.Model):
     name = models.CharField(max_length=40)
     maxNumberOfPeople = models.IntegerField()
-    building = models.ForeignKey('Building', on_delete=models.CASCADE)
-    type = models.ForeignKey('RoomType', on_delete=models.CASCADE)
-    softwares = models.ManyToManyField(Software, through='SoftwareRoom')
-    equipments = models.ManyToManyField(Equipment, through='EquipmentRoom')
-    room_keepers = models.ManyToManyField(User, through='RoomKeeper')
+    building = models.ForeignKey("Building", on_delete=models.CASCADE)
+    type = models.ForeignKey("RoomType", on_delete=models.CASCADE)
+    softwares = models.ManyToManyField(Software, through="SoftwareRoom")
+    equipments = models.ManyToManyField(Equipment, through="EquipmentRoom")
+    room_keepers = models.ManyToManyField(User, through="RoomKeeper")
     available = models.BooleanField(default=True)
 
     def get_absolute_url(self):
-        return reverse('rezerwacje:room_detail',
-                       args=[self.name, self.slug])
+        return reverse("rezerwacje:room_detail", args=[self.name, self.slug])
 
     def __str__(self):
-       return self.name
+        return self.name
 
 
 class Building(models.Model):
     name = models.CharField(max_length=40)
     slug = models.SlugField(max_length=100, unique=True)
-    coordinators = models.ManyToManyField(User, through='Coordinator')
+    coordinators = models.ManyToManyField(User, through="Coordinator")
 
     def get_absolute_url(self):
-        return reverse('rezerwacje:building_detail',
-                       args=[self.name,
-                             self.slug])
+        return reverse("rezerwacje:building_detail", args=[self.name, self.slug])
+
     def __str__(self):
-       return self.name
+        return self.name
 
 
 class RoomType(models.Model):
     name = models.CharField(max_length=40)
 
     def __str__(self):
-       return self.name
+        return self.name
 
 
 class SoftwareRoom(models.Model):
     amount = models.IntegerField()
-    software = models.ForeignKey('Software', on_delete=models.CASCADE)
-    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    software = models.ForeignKey("Software", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
 
 
 class EquipmentRoom(models.Model):
     amount = models.IntegerField()
     amount_of_free_equipment = models.IntegerField()
-    equipment = models.ForeignKey('Equipment', on_delete=models.CASCADE)
-    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    equipment = models.ForeignKey("Equipment", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
 
 
 class Coordinator(models.Model):
-    building = models.ForeignKey('Building', on_delete=models.CASCADE)
+    building = models.ForeignKey("Building", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class RoomKeeper(models.Model):
-    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-
-
-
--- users/models.py
+++ users/models.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
-from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField(_('email address'), unique=True)
+    USER_TYPES = (
+        ('coordinator', 'Coordinator'),
+        ('room_keeper', 'Room Keeper'),
+        ('student', 'Student'),
+        ('teacher', 'Teacher')
+    )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
+    status = models.CharField(max_length=12,
+                              choices=USER_TYPES,
+                              default='waiting')
 
     def __str__(self):
-        return self.email
+        return self.username
